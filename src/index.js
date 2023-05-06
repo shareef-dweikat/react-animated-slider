5a8d145858141fbe9d6e883a69e370d4990b2208
shareef-dweikat <shareef.dweikat@gmail.com>
2023-05-06 17:35:23 +0300

    add build folder

--- src/index.js
+++ src/index.js
@@ -288,8 +288,8 @@ class Slider extends React.PureComponent {
 		const {
 			children,
 			className = 'slider',
-			previousButton = <Arrow direction={this.direction === HORIZONTAL ? 'left' : 'down'} />,
-			nextButton = <Arrow direction={this.direction === HORIZONTAL ? 'right' : 'up'} />,
+			previousButton = <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z"/></svg></div>,
+			nextButton = <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg></div>,
 			touchDisabled,
 			autoplay,
 		} = this.props;
